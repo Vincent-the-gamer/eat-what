f0afefc9252d3e25afb5a24de4d761e764fb91e5
Vincent-the-gamer <erxianqiaottjt@163.com>
2025-08-13 09:38:30 +0800

    feat: support PWA.

--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,7 @@ import VueMacros from 'unplugin-vue-macros/vite'
 import { VueRouterAutoImports } from 'unplugin-vue-router'
 import VueRouter from 'unplugin-vue-router/vite'
 import { defineConfig } from 'vite'
+import { VitePWA } from "vite-plugin-pwa"
 
 export default defineConfig({
   server: {
@@ -54,5 +55,71 @@ export default defineConfig({
       dts: true,
     }),
     UnoCSS(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      devOptions: {
+        enabled: true,
+      },
+      injectRegister: "auto",
+      manifest: {
+        name: "Eat What",
+        short_name: "Eat What",
+        description: "What to eat this morning/noon/dusk?",
+        icons: [
+          {
+            src: "/pwa/icons/icon-48x48.png",
+            sizes: "48x48",
+            type: "image/png"
+          },
+          {
+            src: "/pwa/icons/icon-72x72.png",
+            sizes: "72x72",
+            type: "image/png"
+          },
+          {
+            src: "/pwa/icons/icon-96x96.png",
+            sizes: "96x96",
+            type: "image/png"
+          },
+          {
+            src: "/pwa/icons/icon-128x128.png",
+            sizes: "128x128",
+            type: "image/png"
+          },
+          {
+            src: "/pwa/icons/icon-144x144.png",
+            sizes: "144x144",
+            type: "image/png"
+          },
+          {
+            src: "/pwa/icons/icon-152x152.png",
+            sizes: "152x152",
+            type: "image/png"
+          },
+          {
+            src: "/pwa/icons/icon-192x192.png",
+            sizes: "192x192",
+            type: "image/png"
+          },
+          {
+            src: "/pwa/icons/icon-256x256.png",
+            sizes: "256x256",
+            type: "image/png"
+          },
+          {
+            src: "/pwa/icons/icon-384x384.png",
+            sizes: "384x384",
+            type: "image/png"
+          },
+          {
+            src: "/pwa/icons/icon-512x512.png",
+            sizes: "512x512",
+            type: "image/png"
+          }
+        ],
+        start_url: "/",
+        display: "standalone",
+      },
+    })
   ],
 })
